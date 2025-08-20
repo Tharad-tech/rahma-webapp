9a715437c5bf0987dff14a37869b69f6e71a9e25
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-20 19:29:53 +0300

    change state section info update the style classes to display design as figma design

--- app/components/Home/StateSection.vue
+++ app/components/Home/StateSection.vue
@@ -4,12 +4,11 @@ import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 
 const stats = computed(() => [
-  { icon: '/img/icons/star.svg', value: 1200, label: t('landing.state.comments') },
-  { icon: '/img/icons/earth.svg', value: 7000, label: t('landing.state.active_users') },
-  { icon: '/img/icons/group.svg', value: 1500, label: t('landing.state.delegates') },
-  { icon: '/img/icons/download.svg', value: 18000, label: t('landing.state.downloads') },
+  { icon: '/img/icons/download.svg', value: 20, label: t('landing.stats.app_downloads') },
+  { icon: '/img/icons/group.svg', value: 20, label: t('landing.stats.active_users') },
+  { icon: '/img/icons/hospital.svg', value: 20, label: t('landing.stats.providers') },
+  { icon: '/img/icons/star.svg', value: 20, label: t('landing.stats.ratings') },
 ],
-
 )
 </script>
 
@@ -19,25 +18,31 @@ const stats = computed(() => [
     class="py-16"
   >
     <div class="container">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 text-center py-14">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-16  lg:gap-y-24 lg:gap-x-30 text-center py-14">
         <div
           v-for="(stat, index) in stats"
           :key="index"
-          class="flex flex-col items-center gap-4"
-          :class="{ 'lg:border-e': index !== stats.length - 1 }"
+         
+          class="flex flex-col items-center gap-3"
+:class="{
+  'md:border-e': index % 2 === 0  && index !== stats.length - 1,
+  'lg:border-e': index % 2 !== 0 && index !== stats.length - 1,
+  'order-first lg:order-none ': index ==2 ,
+  'order-3 lg:order-none md:border-s lg:border-s-0 md:border-e-0 lg:border-e': index==0 
+}"
           data-aos="fade-up"
           data-aos-delay="100"
         >
           <NuxtImg
             :src="stat.icon"
             alt="Icon for state"
-            class="text-primary size-[61px]"
+            class="text-primary size-[50px]"
           />
           <CounterAnimation
             :number="stat.value"
-            class="text-[36px] font-bold text-primary mt-2"
+            class="text-[24px] font-bold text-primary mt-2"
           />
-          <span class="text-primary text-[26px]">{{ stat.label }}</span>
+          <span class="text-primary md:text-[32px] text-[16px]">{{ stat.label }}</span>
         </div>
       </div>
     </div>
