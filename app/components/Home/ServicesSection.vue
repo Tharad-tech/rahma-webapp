48d78ea5d864feed672660c48cdb490257e6d14a
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-20 11:27:35 +0300

    update service section to rahma info  and change styles for response

--- app/components/Home/ServicesSection.vue
+++ app/components/Home/ServicesSection.vue
@@ -4,10 +4,10 @@ import { useI18n } from 'vue-i18n'
 const { t, locale } = useI18n()
 
 const services = [
-  { key: 'remote_booking', icon: '/img/icons/suit.svg' },
-  { key: 'call_or_video', icon: '/img/icons/repairing.svg' },
-  { key: 'certified_professionals', icon: '/img/icons/engineering.svg' },
-  { key: 'privacy_security', icon: '/img/icons/delivery-fast-thin.svg' },
+  { key: 'remote_booking', icon: '/img/icons/success-icon.svg' },
+  { key: 'call_or_video', icon: '/img/icons/success-icon.svg' },
+  { key: 'certified_professionals', icon: '/img/icons/success-icon.svg' },
+  { key: 'privacy_security', icon: '/img/icons/success-icon.svg' },
 ]
 </script>
 
@@ -17,42 +17,42 @@ const services = [
     class="py-16"
   >
     <div class="container">
-      <div class="grid grid-cols-1 lg:grid-cols-5 gap-10 justify-between">
+      <div class="grid grid-cols-1 lg:grid-cols-5 gap-24 justify-evenly">
         <!-- image -->
         <div class="hidden lg:col-span-2 lg:flex justify-center">
           <NuxtImg
             data-aos="fade-up"
             :src="locale === 'ar' ? '/img/screens/services-screen-ar.png' : '/img/screens/services-screen-en.png'"
             alt="App preview"
-            height="660"
-            width="327"
+            height="464.18"
+            width="468"
             format="webp"
           />
         </div>
 
         <!-- services -->
-        <div class="lg:col-span-3 text-center lg:text-start -mt-4 lg:mt-4">
-          <h2 class="title-underline text-primary text-2xl md:text-4xl font-bold inline-block mb-11">
+        <div class="lg:col-span-3 text-center lg:text-start lg:mt-4">
+          <h2 class="text-primary text-2xl md:text-4xl font-bold mb-7 border-b-4  border-[#6ACEE5] w-fit pb-3 ">
             {{ t('landing.service.our_services') }}
           </h2>
           <p
             data-aos="fade-up"
-            class="text-muted-foreground text-xl font-medium mb-20"
+            class="text-secondary-foreground text-lg mb-10 md:w-5/6"
           >
             {{ t('landing.service.services_description') }}
           </p>
-          <div class="md:grid  md:grid-cols-2 gap-y-20 gap-x-28 mt-6 flex flex-col items-center justify-center">
+          <div class=" mt-6 flex flex-col items-start gap-6 justify-center">
             <div
               v-for="(service, index) in services"
               :key="index"
-              class="flex items-center md:flex-row flex-col gap-6"
+              class="flex items-center md:flex-row flex-col gap-5"
               data-aos="fade-up"
               data-aos-delay="100"
             >
-              <div class="bg-primary p-3 rounded-md">
+              <div>
                 <NuxtImg
                   :src="service.icon"
-                  alt="Service icon"
+                  alt="success-icon "
                   class="w-8 h-8"
                   width="32"
                   height="32"
@@ -60,7 +60,7 @@ const services = [
                 />
               </div>
               <div>
-                <p class="text-[24px]">
+                <p class="text-[24px] text-[#121212]">
                   {{ t(`landing.service.${service.key}`) }}
                 </p>
               </div>
