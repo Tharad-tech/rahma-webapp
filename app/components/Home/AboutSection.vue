ca4a8698933a61362deab142e58a474a52b53960
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-20 13:49:23 +0300

    change about section details with rahma info , add the style classes for design

--- app/components/Home/AboutSection.vue
+++ app/components/Home/AboutSection.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 const { t, locale } = useI18n()
+const aboutServices = [
+  { key: 'feature1', icon: '/img/icons/success-icon.svg' },
+  { key: 'feature2', icon: '/img/icons/success-icon.svg' },
+  { key: 'feature3', icon: '/img/icons/success-icon.svg' },
+  { key: 'feature4', icon: '/img/icons/success-icon.svg' },
+  { key: 'feature5', icon: '/img/icons/success-icon.svg' },
+]
 </script>
 
 <template>
   <section
     id="about"
-    class="py-16 bg-secondary/10 relative before:content-[''] before:absolute before:inset-0 before:z-[-1] before:bg-no-repeat before:bg-[top_right] ltr:before:-scale-x-100"
+    class="py-16 bg-secondary/10"
   >
     <div class="container">
+    
       <div
-        class="lg:grid lg:grid-cols-2 gap-10 flex justify-center items-center flex-col"
+        class="lg:grid lg:grid-cols-2 gap-24 flex justify-center items-center flex-col-reverse"
       >
+        <!-- image  -->
         <div class="flex">
           <NuxtImg
             data-aos="fade-up"
@@ -21,88 +30,51 @@ const { t, locale } = useI18n()
             "
             alt="About Image"
             format="webp"
-            width="488"
-            height="611"
+            width="484"
+            height="422"
           />
         </div>
-        <div class="text-center lg:text-start">
-          <span class="text-muted-foreground text-2xl mb-7">{{
-            t("landing.about.span")
-          }}</span>
-          <div class="text-4xl 4xl:text-5xl font-bold text-primary mb-16 pt-7">
+        <!-- content -->
+        <div class="text-start">
+         
+          <div class="text-4xl 4xl:text-5xl font-bold text-primary mb-8 pt-7">
             <h2
-              class="text-neutral-800 inline-block text-pretty leading-relaxed"
-            >
-              <span class="title-underline">{{
-                t("landing.about.highlight")
-              }}</span>
-              <span>
-                {{ t("landing.about.title") }}
-              </span>
+              class="text-primary inline-block text-pretty leading-relaxed mb-4"
+            >              
+                {{ t("landing.about.title") }}     
             </h2>
+            <p
+              class="text-muted-foreground mb-10 text-lg"
+              data-aos="fade-up"
+            >
+              {{ t("landing.about.description") }}
+            </p>
           </div>
-          <p
-            class="text-muted-foreground mb-10 text-xl"
-            data-aos="fade-up"
-          >
-            {{ t("landing.about.description") }}
-          </p>
 
-          <div class="text-neutral-800 font-medium text-2xl">
-            <ul class="grid grid-cols-1 md:grid-cols-2 gap-2">
-              <li
-                class="flex items-center gap-8"
-                data-aos="fade-up"
-                data-aos-delay="100"
-              >
-                <Icon
-                  name="clarity:success-line"
-                  class="text-secondary"
-                  width="16"
-                  height="10"
+          <div class=" mt-6 flex flex-col items-start gap-6 justify-center">
+            <div
+              v-for="(service, index) in aboutServices"
+              :key="index"
+              class="flex items-center md:flex-row  gap-5"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <div>
+                <NuxtImg
+                  :src="service.icon"
+                  alt="success-icon "
+                  class="w-10 h-10"
+                  width="40"
+                  height="40"
+                  format="webp"
                 />
-                {{ t("landing.about.feature2") }}
-              </li>
-              <li
-                class="flex items-center gap-8"
-                data-aos="fade-up"
-                data-aos-delay="100"
-              >
-                <Icon
-                  name="clarity:success-line"
-                  class="text-secondary"
-                  width="16"
-                  height="10"
-                />
-                {{ t("landing.about.feature1") }}
-              </li>
-              <li
-                class="flex items-center gap-8"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                <Icon
-                  name="clarity:success-line"
-                  class="text-secondary"
-                  width="16"
-                  height="10"
-                />
-                {{ t("landing.about.feature3") }}
-              </li>
-              <li
-                class="flex items-center gap-8"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                <Icon
-                  name="clarity:success-line"
-                  class="text-secondary"
-                  width="16"
-                  height="10"
-                />
-                {{ t("landing.about.feature4") }}
-              </li>
-            </ul>
+              </div>
+              <div>
+                <p class="text-[24px] text-[#121212]">
+                  {{ t(`landing.about.${service.key}`) }}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
