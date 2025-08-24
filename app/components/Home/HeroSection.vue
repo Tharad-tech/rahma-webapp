91f744393c8e8cf71fc707dc528e52ad2885c7d6
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-24 21:54:05 +0300

    update font size , image sizes in all sections

--- app/components/Home/HeroSection.vue
+++ app/components/Home/HeroSection.vue
@@ -14,18 +14,18 @@ const { t, locale } = useI18n()
         <!-- content -->
         <div class="text-start">
           <h1
-            class="text-[40px] 4xl:text-3xl font-bold text-primary mb-6"
+            class="text-2xl lg:text-[40px] 4xl:text-3xl font-bold text-primary mb-6"
           >
             {{ t("landing.hero.app_name") }}
           </h1>
-          <div class="text-[16px] lg:text-xl 4xl:text-3xl text-secondary">
+          <div class="text-xs lg:text-base 4xl:text-3xl text-secondary">
             <p class="my-6">
               {{ t("landing.hero.app_description") }}
             </p>
           </div>
 
           <div
-            class="flex justify-center lg:justify-start  gap-4 mt-10 lg:mt-20"
+            class="flex justify-center lg:justify-start flex-wrap gap-4 mt-10 lg:mt-20"
           >
             <GooglePlayBadge class="!bg-[#6ACEE5]" />
             <AppStoreBadge class="!bg-[#6ACEE5]" />
