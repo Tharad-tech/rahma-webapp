06b51638239355055bbd37dc4851a8e23ca0fc41
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-27 22:53:16 +0300

    remove useAppSettings composable and refactor API plugin to enhance configuration handling and language support

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -1,21 +1,17 @@
-// plugins/app-config.ts
-export default defineNuxtPlugin(async () => {
-  const { data } = await useFetch('https://rahma.tharadtech.com/api/settings')
-  
-  if (data.value) {
-    const appConfig = useAppConfig()
-    // Merge API data with local config
-    const mergedConfig = {
-      ...appConfig,
-      ...data.value,
-      // Preserve nested structures
-      author: { ...appConfig.author, ...data.value?.author },
-      app: { ...appConfig.app, ...data.value?.app },
-      contact: { ...appConfig.contact, ...data.value?.contact }
-    }
-    
-    // Update runtime config
-    const runtimeConfig = useRuntimeConfig()
-    runtimeConfig.app = mergedConfig
+export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig()
+  const api = $fetch.create({
+    baseURL: config.public.apiBase,
+    onRequest({ options }) {
+      const lang = useCookie('i18n_locale').value || 'en'
+      options.headers.set('Accept-Language', lang)
+    },
+  })
+
+  // Expose to useNuxtApp().$api
+  return {
+    provide: {
+      api,
+    },
   }
 })
